7327877caf3b05e9f362c315f1d1b834394bd60a
Mufid Alnadi <mufidalnadi@gmail.com>
2023-06-15 22:12:18 +0300

    push

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,14 +1,15 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 
 function App() {
-
   return (
-<h1 className="text-3xl font-bold underline">
-      Hello world!
-    </h1> 
-    
-    
-    )
+    <BrowserRouter>
+       
+      <Routes>
+     
+      </Routes>
+    </BrowserRouter>
+  );
 }
 
-export default App
+export default App;
--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -1,5 +1,9 @@
 /** @type {import('tailwindcss').Config} */
-module.exports = {
+const withMT = require("@material-tailwind/react/utils/withMT");
+
+
+
+module.exports =withMT( {
   content: [
     "./app/**/*.{js,ts,jsx,tsx,mdx}",
     "./pages/**/*.{js,ts,jsx,tsx,mdx}",
@@ -16,4 +20,4 @@ module.exports = {
     require('flowbite/plugin')
 
   ],
-}
+});
