0460cfca8618073597617559d19ff2c4196fac4d
Mufid Alnadi <mufidalnadi@gmail.com>
2023-06-15 22:54:11 +0300

    data base

--- backend/src/config/config.js
+++ backend/src/config/config.js
@@ -8,7 +8,7 @@ const connectionParams = {
 }
 const MONGO_USERNAME = process.env.MONGO_USERNAME || '';
 const MONGO_PASSWORD = process.env.MONGO_PASSWORD || '';
-const MONGO_URL = `mongodb+srv://${MONGO_USERNAME}:${MONGO_PASSWORD}@cluster0.ynmawcy.mongodb.net/?retryWrites=true&w=majority`;
+const MONGO_URL = `mongodb+srv://${MONGO_USERNAME}:${MONGO_PASSWORD}@cluster0.ynmawcy.mongodb.net/ScholarFund?retryWrites=true&w=majority`;
 
 module.exports = {
   MONGO_URL,
