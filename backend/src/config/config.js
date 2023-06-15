0ad81937b4d05e4e5fecf2c2c2a3005259e254d9
Mufid Alnadi <mufidalnadi@gmail.com>
2023-06-15 19:08:23 +0300

    data base

---
+++ backend/src/config/config.js
@@ -0,0 +1,16 @@
+const dotenv = require('dotenv');
+
+dotenv.config();
+
+const connectionParams = {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+}
+const MONGO_USERNAME = process.env.MONGO_USERNAME || '';
+const MONGO_PASSWORD = process.env.MONGO_PASSWORD || '';
+const MONGO_URL = `mongodb+srv://${MONGO_USERNAME}:${MONGO_PASSWORD}@cluster0.ynmawcy.mongodb.net/?retryWrites=true&w=majority`;
+
+module.exports = {
+  MONGO_URL,
+  connectionParams,
+};
