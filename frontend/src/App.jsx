ab01e1b1424a173c22820802c53df9150b518156
MufidAlnadi <mufidalnadi@gmail.com>
2023-06-16 00:36:37 +0300

    header footer

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,14 +1,19 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Navbar from "./components/Navbar";
+import Footer from "./components/Footer";
 
 
 function App() {
   return (
+       // testing
     <BrowserRouter>
-       
+    <Navbar/>
       <Routes>
      
       </Routes>
+      <Footer/>
     </BrowserRouter>
+
   );
 }
 
